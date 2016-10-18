aa83959f9eac910bcdf6f51293d0c6b6683dc781
bori0066 <borisgrunwald@gmail.com>
2016-10-18 17:00:19 +0200

    initial

--- src/main/java/RunProgram.java
+++ src/main/java/RunProgram.java
@@ -13,50 +13,30 @@ public class RunProgram {
 
     public static void main(String[] args) {
 
-        Random r = new Random();
-
-        String[] list = new String[10];
-
-        list[0] = "lul";
-        list[1] = "hee";
-        list[2] = "hva";
-        list[3] = "ncie";
-        list[4] = "drgdr";
-        list[5] = "huhu";
-        list[6] = "yea";
-        list[7] = "wow";
-        list[8] = "suchstring";
-        list[9] = "hvafaen";
-
-        Lock
+        String time = "12:00:00AM";
 
 
 
-        long startTime = System.currentTimeMillis();
-
-        Sort.quickSort(list, new Comparator<String>() {
-            public int compare(String o1, String o2) {
-                return o1.length()-o2.length();
-            }
-        });
-
-        long endTime = System.currentTimeMillis();
-
-
-        System.out.println(Arrays.toString(list));
+        System.out.println(time(time));
 
 
 
+    }
 
 
 
+    public static String time(String time) {
 
+        String hours = null;
 
-
-
-
-
-
+        if(time.substring(time.length()-2).equals("PM")) {
+            if(time.substring(0,2).equals("12")) {
+                hours = "00";
+            } else hours = String.valueOf(Integer.valueOf(time.substring(0,2))+12);
+            return hours+time.substring(2,time.length()-2);
+        } else {
+            return time.substring(0, time.length() - 2);
+        }
 
     }
 
